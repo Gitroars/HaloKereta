bb2308683c8a3679c337d2c4b72d335414aa481a
Gitroars <yuwonoarvin@gmail.com>
2022-12-23 10:19:54 +0700

    Uncomment codes

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -43,7 +43,7 @@ public class DatabaseManager : MonoBehaviour
 
     #endregion
 
-    #region METHODS
+    
     
    
 
@@ -62,165 +62,163 @@ public class DatabaseManager : MonoBehaviour
     }
     
     
-    
-    
-
-    // private void AddRoutes()
-    // {
-    //     
-    //     using var cmd = new MySqlCommand();
-    //     cmd.Connection = connection;
-    //     
-    //     
-    //     // Read text file
-    //     foreach (string line in System.IO.File.ReadLines(path))
-    //     {
-    //         List<string> data = new List<string>(line.Split(','));
-    //         string origin = data[0];
-    //         string destination = data[1];
-    //         string cost = data[2];
-    //         
-    //         
-    //         try
-    //         {
-    //             print("About to establish connection");
-    //             using (connection)
-    //             {
-    //                 connection.Open();
-    //                 print("MySQL - Opened Connection");
-    //                 cmd.CommandText = $"SHOW DATABASES";
-    //                     // $"INSERT IGNORE INTO routes (station_origin,station_destination,price) VALUES('{origin}','{destination}',{cost})";
-    //                     
-    //                          
-    //                 cmd.ExecuteNonQuery();
-    //             }
-    //         }
-    //         catch (MySqlException exception)
-    //         {
-    //             print(exception.Message);
-    //         }
-    //         
-    //
-    //         
-    //     }
-    //
-    //     print("Successfully added new routes");
-    //
-    //
-    // }
-    //
-    // private void AddMachinist()
-    // {
-    //     
-    //     using var cmd = new MySqlCommand();
-    //     cmd.Connection = connection;
-    //     
-    //     
-    //     // Read text file
-    //     foreach (string line in System.IO.File.ReadLines(machinist))
-    //     {
-    //         List<string> data = new List<string>(line.Split(','));
-    //         string name = data[0];
-    //         
-    //         
-    //         try
-    //         {
-    //             using (connection)
-    //             {
-    //                 connection.Open();
-    //                 print("MySQL - Opened Connection");
-    //                 cmd.CommandText =
-    //                     $"INSERT IGNORE INTO machinist (machinist_name) VALUES('{name}')";
-    //                     
-    //                          
-    //                 cmd.ExecuteNonQuery();
-    //             }
-    //         }
-    //         catch (MySqlException exception)
-    //         {
-    //             print(exception.Message);
-    //         }
-    //         
-    //
-    //         
-    //     }
-    //
-    //     print("Successfully added new machinist");
-    //
-    //
-    // }
-    //
-    //
-    // private void AddTrain()
-    // {
-    //     
-    //     using var cmd = new MySqlCommand();
-    //     cmd.Connection = connection;
-    //     
-    //     
-    //     // Read text file
-    //     foreach (string line in System.IO.File.ReadLines(train))
-    //     {
-    //         List<string> data = new List<string>(line.Split(','));
-    //         string rail = data[0];
-    //         string type = data[1];
-    //         string carriage = data[2];
-    //
-    //
-    //         try
-    //         {
-    //             using (connection)
-    //             {
-    //                 connection.Open();
-    //                 print("MySQL - Opened Connection");
-    //                 cmd.CommandText =
-    //                     $"INSERT IGNORE INTO train (railway,train_type,carriage) VALUES('{rail}','{type}',{carriage})";
-    //                     
-    //                          
-    //                 cmd.ExecuteNonQuery();
-    //             }
-    //         }
-    //         catch (MySqlException exception)
-    //         {
-    //             print(exception.Message);
-    //         }
-    //         
-    //
-    //         
-    //     }
-    //
-    //     print("Successfully added new train");
-    //
-    //
-    // }
-    //
-    //
-    //
-    // public void AddUser(string phoneNumber, string fullName, string email,string pin)
-    // {
-    //     
-    //     try
-    //     {
-    //         using (connection)
-    //         {
-    //             connection.Open();
-    //             using var cmd = new MySqlCommand();
-    //             cmd.Connection = connection;
-    //             cmd.CommandText = $"INSERT IGNORE INTO users (name,email,phone_number,pin) VALUES ('{fullName}','{email}','{phoneNumber}','{pin}')";
-    //             cmd.ExecuteNonQuery();
-    //             print("MySQL - Opened Connection");
-    //         }
-    //     }
-    //     catch (MySqlException exception)
-    //     {
-    //         print(exception.Message);
-    //     }
-    //     
-    // }
-    
+    #region DATABASE SETUP
     
 
+    private void AddRoutes()
+    {
+        
+        using var cmd = new MySqlCommand();
+        cmd.Connection = connection;
+        
+        
+        // Read text file
+        foreach (string line in System.IO.File.ReadLines(path))
+        {
+            List<string> data = new List<string>(line.Split(','));
+            string origin = data[0];
+            string destination = data[1];
+            string cost = data[2];
+            
+            
+            try
+            {
+                print("About to establish connection");
+                using (connection)
+                {
+                    connection.Open();
+                    print("MySQL - Opened Connection");
+                    cmd.CommandText = $"SHOW DATABASES";
+                        // $"INSERT IGNORE INTO routes (station_origin,station_destination,price) VALUES('{origin}','{destination}',{cost})";
+                        
+                             
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException exception)
+            {
+                print(exception.Message);
+            }
+            
     
-
+            
+        }
+    
+        print("Successfully added new routes");
+    
+    
+    }
+    //
+    private void AddMachinist()
+    {
+        
+        using var cmd = new MySqlCommand();
+        cmd.Connection = connection;
+        
+        
+        // Read text file
+        foreach (string line in System.IO.File.ReadLines(machinist))
+        {
+            List<string> data = new List<string>(line.Split(','));
+            string name = data[0];
+            
+            
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    print("MySQL - Opened Connection");
+                    cmd.CommandText =
+                        $"INSERT IGNORE INTO machinist (machinist_name) VALUES('{name}')";
+                        
+                             
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException exception)
+            {
+                print(exception.Message);
+            }
+            
+    
+            
+        }
+    
+        print("Successfully added new machinist");
+    
+    
+    }
+    
+    
+    private void AddTrain()
+    {
+        
+        using var cmd = new MySqlCommand();
+        cmd.Connection = connection;
+        
+        
+        // Read text file
+        foreach (string line in System.IO.File.ReadLines(train))
+        {
+            List<string> data = new List<string>(line.Split(','));
+            string rail = data[0];
+            string type = data[1];
+            string carriage = data[2];
+    
+    
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    print("MySQL - Opened Connection");
+                    cmd.CommandText =
+                        $"INSERT IGNORE INTO train (railway,train_type,carriage) VALUES('{rail}','{type}',{carriage})";
+                        
+                             
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException exception)
+            {
+                print(exception.Message);
+            }
+            
+    
+            
+        }
+    
+        print("Successfully added new train");
+    
+    
+    }
     #endregion
+    public void AddUser(string phoneNumber, string fullName, string email,string pin)
+    {
+        
+        try
+        {
+            using (connection)
+            {
+                connection.Open();
+                using var cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = $"INSERT IGNORE INTO users (name,email,phone_number,pin) VALUES ('{fullName}','{email}','{phoneNumber}','{pin}')";
+                cmd.ExecuteNonQuery();
+                print("MySQL - Opened Connection");
+            }
+        }
+        catch (MySqlException exception)
+        {
+            print(exception.Message);
+        }
+        
+    }
+    
+    
+
+    
+
+   
 }
